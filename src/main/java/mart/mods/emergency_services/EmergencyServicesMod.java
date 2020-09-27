021d92f83728c7f3fcb4a0284b91f8ad1bfb321a
Matt Taylor <mtaylor@adzerk.com>
2020-09-27 19:09:19 +0100

    Remove import

--- src/main/java/mart/mods/emergency_services/EmergencyServicesMod.java
+++ src/main/java/mart/mods/emergency_services/EmergencyServicesMod.java
@@ -3,7 +3,6 @@ package mart.mods.emergency_services;
 import mart.mods.emergency_services.items.BatonItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
@@ -20,7 +19,6 @@ public class EmergencyServicesMod implements ModInitializer {
         // Proceed with mild caution.
 
         Registry.register(Registry.ITEM, new Identifier("police", "fabric_item"), baton);
-
 
         System.out.println("Hello Fabric world!");
     }
